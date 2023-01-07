d60f36531b172cc1d1a619efc7848ef9411cb601
Rafipranata <banjardawa34@gmail.com>
2023-01-07 12:18:57 +0700

    first commit

---
+++ Script.js
@@ -0,0 +1,12 @@
+let result = document.getElementById('screen')
+		const display = (a) => {
+			if (a == 'clear') {
+				result.value = ''
+			} else if (a == 'del') {
+				result.value = result.value.slice(0,-1)
+			} else if (a == 'action') {
+				result.value = eval(result.value)
+			} else{
+				result.value += a
+			}
+		}
